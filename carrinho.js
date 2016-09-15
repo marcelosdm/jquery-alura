2cb87248345eebb54e83f52bf73d0c905bc0ea2e
Marcelo Melo STA <marcelosdm@gmail.com>
2016-09-15 09:47:15 -0300

    Finalização do Curso

--- carrinho.js
+++ carrinho.js
@@ -1,32 +1,88 @@
-		var atualizaDados = function(){
-			var itens = $(".item-total");
-			var total = 0;
-			for(var i = 0; i < itens.length; i++){
-				var item = $(itens[i]);
-				var preco = parseFloat(item.text());
-				total = total + preco;
-			}
-			$("#valor-total").text(total);
-			$("#quantidade-de-itens").text(itens.length);
-		};
+var umaPropaganda = function(){
+    var propagandas = ["O que acha de comprar uma motocicleta?",
+               "O que acha de comprar uma lancha?",
+               "O que acha de comprar uma bicicleta?",
+               "O que acha de comprar uma carro?"
+               ];
 
-		var removeItem = function(event){
-			event.preventDefault();
-			
-			var self = $(this);
-			self.closest("tr").hide();
-			atualizaDados();
+    var posicao = Math.floor(propagandas.length *Math.random());
+    var texto = propagandas[posicao];
+    var tr =$("<tr>").addClass("propaganda").append($("<td>"));
+    tr.find("td").attr("colspan",6).text(texto);
+    return tr;
+}
 
-			var undo = function(){
-				var trs = $('tr:hidden');
-				trs.show();
-			}
-		};
 
-		var aposInicializado = function(){
-			$(".remove-item").click(removeItem);
-			atualizaDados();
-			$("#undo").click(undo);
+var atualizaDados = function(){
+	var carrinhos = $('.carrinho');
+	carrinhos.each(function(){
+		var carrinho = $(this);
+	
+		var itens = carrinho.find(".item-total:visible");
+		var total = 0;
+		for(var i = 0; i < itens.length; i++){
+			var item = $(itens[i]);
+			var preco = parseFloat(item.text());
+			total = total + preco;
 		}
+		carrinho.find(".valor-total").text(total);
+		carrinho.find(".quantidade-de-itens").text(itens.length);
+	});
+};
 
-		$(aposInicializado);
+var removeItem = function(event){
+	event.preventDefault();
+	
+	var self = $(this);
+	self.closest("tr").hide();
+	atualizaDados();
+
+};
+
+var undo = function(){
+	var carrinho = $(this).closest('.carrinho');
+	carrinho.find("tr:visible").removeClass('recuperado');
+		var trs = carrinho.find('tr:hidden');
+	trs.addClass('recuperado');
+	trs.show();
+	atualizaDados();
+};
+
+
+var daDestaque = function(){
+	$(this).addClass('hovering');
+	$(this).find(".remove-item").fadeIn();	
+};
+
+var tiraDestaque = function(){
+	$(this).removeClass('hovering');
+	$(this).find(".remove-item").fadeOut();
+
+};
+
+
+var alternaPropagandas = function(event){
+	event.preventDefault();
+	$(".propaganda").fadeToggle(1000);
+	$(".alterna-propaganda").toggle();
+};
+
+var aposInicializado = function(){
+	atualizaDados();
+	$(".undo").click(undo);
+	$(".remove-item").click(removeItem);
+
+	$(".carrinho").each(function(){
+		$(this).find("tr:nth-child(3n), tr:last").each(function(){
+			umaPropaganda().insertAfter($(this));
+		});
+	});
+
+	$(".item-carrinho").hover(daDestaque, tiraDestaque);	
+
+	$("#esconde-propagandas").click(alternaPropagandas);
+	$("#mostra-propagandas").click(alternaPropagandas);
+
+};
+
+$(aposInicializado);
